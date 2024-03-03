42d89bf9b5cdb3ce5ef707fdfc30f5e6b2d0ace6
hubert kieslich <hubertargo@interia.pl>
2024-03-03 21:20:33 +0100

    Finished the logic behind UpdateKeys and install Nuget package for reading from .env file.

--- Osint_WPF/View/UpdateKeys.xaml.cs
+++ Osint_WPF/View/UpdateKeys.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Osint_WPF
 {
@@ -41,7 +42,19 @@ namespace Osint_WPF
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            //Confirms changes overriting .env file unless no changes
+            if (txtHibpApiKey.Text != "")
+            {
+                SaveInputToEnvFile(txtHibpApiKey.Text, "hibp_ApiKey=");
+            }
+            if (txtDehashedUsername.Text != "")
+            {
+                SaveInputToEnvFile(txtDehashedUsername.Text, "dehashedApiUsername=");
+            }
+            if (txtDehashedApiKey.Text != "")
+            {
+                SaveInputToEnvFile(txtDehashedApiKey.Text, "dehashedApiKey=");
+            }
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -50,6 +63,46 @@ namespace Osint_WPF
             var UpdateKeys = new MainWindow();
             UpdateKeys.Show();
             this.Close();
+        }
+
+        private void SaveInputToEnvFile(string userInput, string dotenvKey)
+        {
+            DirectoryInfo currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+            while (currentDirectory != null && !currentDirectory.GetFiles("*.csproj", SearchOption.TopDirectoryOnly).Any())
+            {
+                currentDirectory = currentDirectory.Parent;
+            }
+
+            if (currentDirectory == null)
+            {
+                MessageBox.Show("Project directory not found.");
+                return;
+            }
+            string envFilePath = System.IO.Path.Combine(currentDirectory.FullName, ".env");
+
+            if (File.Exists(envFilePath))
+            {
+                MessageBox.Show(envFilePath);
+
+                // reads all lines in .env file
+                string[] lines = File.ReadAllLines(envFilePath);
+
+                // updates .env file lines which match the input, to update them
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].StartsWith(dotenvKey))
+                    {
+                        lines[i] = dotenvKey + userInput;
+                        break;
+                    }
+                }
+                File.WriteAllLines(envFilePath, lines);
+            }
+            else
+            {
+                MessageBox.Show(".env file not found.");
+            }
         }
     }
 }
