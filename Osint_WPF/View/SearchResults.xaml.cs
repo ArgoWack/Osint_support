8c8b22aaf4455d3a16e599c7a57640a94c097c63
hubert kieslich <hubertargo@interia.pl>
2024-03-15 00:38:52 +0100

    Added functionality to save results into pdf file

--- Osint_WPF/View/SearchResults.xaml.cs
+++ Osint_WPF/View/SearchResults.xaml.cs
@@ -11,6 +11,12 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.IO;
+using System.Reflection.Metadata;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
 
 namespace Osint_WPF.View
 {
@@ -33,23 +39,45 @@ namespace Osint_WPF.View
         }
         private void DisplayResultsInRichTextBox(string resultsText)
         {
-            var paragraph = new Paragraph();
+            var paragraph = new System.Windows.Documents.Paragraph();
             paragraph.Inlines.Add(new Run(resultsText));
             ResultsRichTextBox.Document.Blocks.Clear();
             ResultsRichTextBox.Document.Blocks.Add(paragraph);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // place for results to be displayed
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //save to pdf
+
+            // creates a new MigraDoc document
+            var document = new MigraDoc.DocumentObjectModel.Document();
+            var section = document.AddSection();
+            var paragraph = section.AddParagraph();
+            paragraph.AddFormattedText(_resultsText, TextFormat.Bold);
+
+            // renders the document to PDF
+            var renderer = new PdfDocumentRenderer(true);
+            renderer.Document = document;
+            renderer.RenderDocument();
+
+            // defines the path to save the PDF
+            string filename = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "OSINTSearchResults.pdf");
+            renderer.PdfDocument.Save(filename);
+
+            MessageBox.Show($"PDF has been saved to: {filename}");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             //Return button
             var BeginDataSearch = new BeginDataSearch();
             BeginDataSearch.Show();
             this.Close();
-        }
-
-        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            // place for results to be displayed
         }
     }
 }
